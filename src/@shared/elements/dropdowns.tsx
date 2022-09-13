6de88b60e1064d0f2b9193236120410b74ea3023
Mustafa <mustafatooook@gmail.com>
2022-09-13 06:54:13 +0300

    started to create heading filter dropdown

--- src/@shared/elements/dropdowns.tsx
+++ src/@shared/elements/dropdowns.tsx
@@ -1,16 +1,20 @@
+import classNames from "classnames"
 import React from "react"
 import { FaChevronRight } from "react-icons/fa"
 
-export const MultiSelect = () => {
-    const [selecteds, setSelecteds] = React.useState('')
-
+export const Dropdown = () => {
+    const [selected, setSelected] = React.useState('')
     return(
-        <div className='flex justify-between items-center w-full rounded-lg bg-white cursor-pointer py-3 px-5'>
+        <div className='relative flex justify-between items-center w-full rounded-lg bg-white cursor-pointer py-3 px-5'>
             <div className="flex items-start">
                 <p>Filtrele:</p>
                 <p className="text-yg-orange mx-2">Sevkiyatı Devam Edenler</p>
             </div>
             <div><FaChevronRight size={17} className='text-gray-400' /> </div>
+            <div className={classNames('dropdown absolute')}>
+
+            </div>
         </div>
     )
 }
+ 
--- src/components/cargo/heading/filter.tsx
+++ src/components/cargo/heading/filter.tsx
@@ -1,21 +1,14 @@
+import { Dropdown } from "@shared/elements/dropdowns"
 import { Search } from "@shared/elements/searches"
 import React from "react"
-import { FaChevronRight } from "react-icons/fa"
 import { Selectes } from "../view"
-
 
 export const FilterHeading = ({wide}:any) => {
 
     return (
         <div className='heading grid grid-cols-2 gap-4'>
             <Search placeholder='İlan ara...' />
-            <div className='flex justify-between items-center w-full rounded-lg bg-white cursor-pointer py-3 px-5'>
-                    <div className="flex items-start">
-                        <p>Filtrele:</p>
-                        <p className="text-yg-orange mx-2">Sevkiyatı Devam Edenler</p>
-                    </div>
-                <div><FaChevronRight size={17} className='text-gray-400' /> </div>
-            </div>
+            <Dropdown />
             {wide && <Selectes />}
         </div>
     )
