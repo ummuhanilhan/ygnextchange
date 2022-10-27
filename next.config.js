df436f3413962ca0bf335b8671b115f5af37bd24
hyperidx <mustafatooook@gmail.com>
2022-10-27 15:06:55 +0300

    eslintrc

--- next.config.js
+++ next.config.js
@@ -22,11 +22,7 @@ const nextConfig = {
 
     return config
   },
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: false,
-  },
+  
 }
 
 module.exports = nextConfig
